6e983308411f06b95afb27e1d03f8ad97f970c88
Pawlo <pawlonazaruk@gmail.com>
2020-11-03 01:34:08 +0100

    Menu bar - scratch
    Gui without functionality

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from view import MyApp
 
 def main():
     root = tk.Tk()
-    root.geometry("800x600")
+    root.geometry("600x600")
     root.title("2048")
     myapp = MyApp(root)
 
--- view.py
+++ view.py
@@ -1,10 +1,18 @@
 import tkinter as tk
+from logic import *
 
 
 class MyApp:
     def __init__(self, root):
         self.root = root
+        self.menu_bar()
         self.main_window()
+
+    def menu_bar(self):
+        menu_bar = tk.Menu(self.root)
+        menu_bar.add_command(label="Restart", command=print("Restart"))
+        menu_bar.add_command(label="Help", command=print("Help"))
+        self.root.config(menu=menu_bar)
 
     def main_window(self):
         self.background = tk.Frame(self.root, bg="red")
