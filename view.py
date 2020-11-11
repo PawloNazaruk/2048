974595153581770768758fbb13c9afc111455aa5
Pawlo <pawlonazaruk@gmail.com>
2020-11-11 18:58:15 +0100

    Created update_board()
    I adjust current styling fields of the board_GUI accroding to actual numbers in board_base to display new view of the board.

--- view.py
+++ view.py
@@ -16,6 +16,9 @@ class MyApp:
         self.make_score()
         self.make_board()
 
+
+        self.update_board()
+
     def make_score(self):
         self.score_frame = tk.Frame(self.root)
         self.score_frame.place(relx=0.5, y=45, anchor="center")
@@ -27,33 +30,35 @@ class MyApp:
     def make_board(self):
         self.grid_frame = tk.Frame(self.root, bg=c.GRID_COLOR)
         self.grid_frame.grid(pady=(100, 0))
-        for row in range(4):
-            board_row = []
-            for col in range(4):
+        for i in range(4):
+            row = []
+            for j in range(4):
                 cell_frame = tk.Frame(self.grid_frame, bg=c.EMPTY_CELL_COLOR, width=140, height=140)
-                cell_frame.grid(row=row, column=col, padx=5, pady=5)
+                cell_frame.grid(row=i, column=j, padx=5, pady=5)
 
                 cell_number = tk.Label(self.grid_frame, bg=c.EMPTY_CELL_COLOR, anchor="center")
-                cell_number.grid(row=row, column=col)
+                cell_number.grid(row=i, column=j)
 
                 cell_data = {"frame": cell_frame, "number": cell_number}
-                board_row.append(cell_data)
-            self.board_GUI.append(board_row)
+                row.append(cell_data)
+            self.board_GUI.append(row)
+
+    def update_board(self):
+        pprint(self.board_base)
+        numbers = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+        for i, row in enumerate(self.board_base):
+            for j in row:
+                if j in numbers:
+                    self.board_GUI[i][j]['frame'].config(bg=c.CELL_COLORS[j])
+                    self.board_GUI[i][j]['number'].config(bg=c.CELL_COLORS[j], text=str(j), font=c.CELL_NUMBER_FONTS[j])
+                else:
+                    self.board_GUI[i][j]['frame'].config(bg=c.EMPTY_CELL_COLOR)
+                    self.board_GUI[i][j]['number'].config(bg=c.EMPTY_CELL_COLOR, text="")
 
     def menu_bar(self):
         menu_bar = tk.Menu(self.root)
         menu_bar.add_command(label="Restart", command=print("Restart"))
         menu_bar.add_command(label="Help", command=print("Help"))
-        self.root.config(menu=menu_bar)
-
-
-
-
-    def generate_cell(self):
-        row = randint(0, 3)
-        col = randint(0, 3)
-        self.cells[row][col]['frame'].config(bg=c.CELL_COLORS[2])
-        self.cells[row][col]['number'].config(bg=c.CELL_COLORS[2], text="2")
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.cells})"
+        return f"{self.__class__.__name__}({self.board_base})"
