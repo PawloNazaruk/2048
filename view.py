45b11fe14fa84a9952f4ccefc4160afe97aa60d7
Pawlo <pawlonazaruk@gmail.com>
2020-11-11 18:37:17 +0100

    Main window - updated namespace
    Further changes in namesspace of the GUI elements and its creating proccess.

--- view.py
+++ view.py
@@ -1,46 +1,44 @@
 import tkinter as tk
 import colors as c
 from game_functionality import *
+from random import randint
+from pprint import pprint
 
 
 class MyApp:
     def __init__(self, root):
         self.root = root
-
-        self.score_frame = tk.Frame(self.root)
-        self.score_frame.place(relx=0.5, y=45, anchor="center")
-
-        self.grid_frame = tk.Frame(self.root, bg=c.GRID_COLOR)
-        self.grid_frame.grid(pady=(100, 0))
+        self.board_GUI = []
+        n = 4
+        self.board_base = [[0 for i in range(n)] for j in range(n)]
 
         self.menu_bar()
-        self.display_score()
-        self.display_board()
+        self.make_score()
+        self.make_board()
 
-        # checks
-        self.cells[0][0]['frame'].config(bg=c.CELL_COLORS[32])
-        self.cells[0][0]['number'].config(bg=c.CELL_COLORS[32], text="32")
+    def make_score(self):
+        self.score_frame = tk.Frame(self.root)
+        self.score_frame.place(relx=0.5, y=45, anchor="center")
+        tk.Label(self.score_frame, text="Score", font=c.SCORE_FONT).grid(row=0)
 
-    def display_score(self):
-        score_frame = tk.Frame(self.root)
-        score_frame.place(relx=0.5, y=45, anchor="center")
-        tk.Label(score_frame, text="Score", font=c.SCORE_FONT).grid(row=0)
-        self.score_label = tk.Label(score_frame, text="0", font=c.SCORE_LABEL_FONT)
+        self.score_label = tk.Label(self.score_frame, text="0", font=c.SCORE_LABEL_FONT)
         self.score_label.grid(row=1)
 
-    def display_board(self):
-        self.cells = []
+    def make_board(self):
+        self.grid_frame = tk.Frame(self.root, bg=c.GRID_COLOR)
+        self.grid_frame.grid(pady=(100, 0))
         for row in range(4):
-            cells_row = []
+            board_row = []
             for col in range(4):
                 cell_frame = tk.Frame(self.grid_frame, bg=c.EMPTY_CELL_COLOR, width=140, height=140)
                 cell_frame.grid(row=row, column=col, padx=5, pady=5)
 
                 cell_number = tk.Label(self.grid_frame, bg=c.EMPTY_CELL_COLOR, anchor="center")
                 cell_number.grid(row=row, column=col)
+
                 cell_data = {"frame": cell_frame, "number": cell_number}
-                cells_row.append(cell_data)
-            self.cells.append(cells_row)
+                board_row.append(cell_data)
+            self.board_GUI.append(board_row)
 
     def menu_bar(self):
         menu_bar = tk.Menu(self.root)
@@ -48,3 +46,14 @@ class MyApp:
         menu_bar.add_command(label="Help", command=print("Help"))
         self.root.config(menu=menu_bar)
 
+
+
+
+    def generate_cell(self):
+        row = randint(0, 3)
+        col = randint(0, 3)
+        self.cells[row][col]['frame'].config(bg=c.CELL_COLORS[2])
+        self.cells[row][col]['number'].config(bg=c.CELL_COLORS[2], text="2")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.cells})"
